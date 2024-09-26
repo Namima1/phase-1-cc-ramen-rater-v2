8d85b31af7550e2d19c3812f6933a2bc5f239bbd
Namima Islam <namimaislam308@gmail.com>
2024-09-25 23:50:02 -0400

    lab

--- src/index.js
+++ src/index.js
@@ -1,24 +1,89 @@
-// index.js
+
+// const addBtn = document.querySelector("#new-ramen-btn");
+// const ramenFormContainer = document.querySelector(".container");
+// const ramenCollection = document.getElementById("ramenCollection");
+// const newRamenForm = document.querySelector("#add-ramen-form")
+// addBtn.addEventListener("click", () => {
+//      addRamen = !addRamen;
+//       if (addRamen) {
+//         ramenFormContainer.style.display = "block";
+//       } else {
+//         ramenFormContainer.style.display = "none";
+//       }
+// });
+
+//submit the form
+
+function displayRamens() {
+  fetch("http://localhost:3000/ramens")
+  .then(response => response.json())
+  .then(ramenArray => ramenArray.forEach(ramen => renderRamen(ramen)))
+};
+
+function renderRamen(ramen) {
+  const ramenImg = document.createElement('img'); 
+  ramenImg.src = ramen.image;
+  ramenImg.alt = ramen.name;
+  ramenImg.addEventListener('click', () => handleClick(ramen))
+
+ const ramenMenu = document.querySelector("#ramen-menu");
+ ramenMenu.append(ramenImg)
+
+
+}
+
+
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
-};
+  const detailImage = document.querySelector("#ramen-detail img"); 
+  detailImage.src = ramen.image;
+
+  const name = document.querySelector("#ramen-detail h2");
+  name.textContent = ramen.name;
+
+  const restaurantName = document.querySelector("#ramen-detail h3");
+  restaurantName.textContent = ramen.restaurant;
+
+  const rating = document.querySelector("#rating-display");
+  rating.textContent = ramen.rating;
+
+  const comment = document.querySelector("#comment-display");
+  comment.textContent = ramen.comment;
+}; 
 
 const addSubmitListener = () => {
-  // Add code
-}
+  const ramenForm = document.querySelector("#new-ramen");
 
-const displayRamens = () => {
-  // Add code
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+     const name = document.querySelector("#new-name").value
+     const restaurant = document.querySelector("#new-restaurant").value
+     const image = document.querySelector("#new-image").value
+     const rating = document.querySelector("#new-rating").value 
+     const comment = document.querySelector("#new-comment").value
+
+     const newRamen = {
+      name,
+      restaurant,
+      image,
+      rating,
+      comment
+     }
+
+     renderRamen(newRamen)
+
+     ramenForm.reset()
+  }
+  ramenForm.addEventListener("submit", handleSubmit)
+}
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+ displayRamens()
+};
 
-main()
+main();
 
 // Export functions for testing
 export {
